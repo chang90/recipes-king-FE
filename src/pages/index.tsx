cfb7954964c3a76993d875ae6dea5a4e5dbce8c9
chang90 <chang.liu.programmer@gmail.com>
2023-12-13 17:09:25 +1100

    play with subgrid demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,16 @@ export default function Page() {
   const { View } = useLottie(options);
   return (
     <main className={styles.main}>
+        <div className={styles.wrapper}>
+          <div className={`${styles.box} ${styles.a}`}>A</div>
+          <div className={`${styles.box} ${styles.b}`}>B</div>
+          <div className={`${styles.box} ${styles.c}`}>C</div>
+          <div className={`${styles.box} ${styles.d}`}>
+            <div className={`${styles.box} ${styles.e}`}>E</div>
+            <div className={`${styles.box} ${styles.f}`}>F</div>
+            <div className={`${styles.box} ${styles.g}`}>G</div>
+          </div>
+      </div>
       <div className={styles.description}>
         <>{View}</>
       </div>
