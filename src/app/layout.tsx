6edce1ac278e821caf0c9d394f5ecf3109f38e92
chang90 <chang.liu.programmer@gmail.com>
2024-01-29 21:17:52 +1100

    feat: add header and footer placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="cn">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer>Footer</footer>
+        </body>
     </html>
   )
 }
