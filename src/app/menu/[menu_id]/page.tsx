dda6bcb38896fca8e29ed578454d0983abe18b59
chang90 <chang.liu.programmer@gmail.com>
2024-01-30 22:41:54 +1100

    fix: change menu id

--- src/app/menu/[menu_id]/page.tsx
+++ src/app/menu/[menu_id]/page.tsx
@@ -1,18 +1,18 @@
 import { Metadata } from "next";
 export const generateMetadata = ({ params }: Props): Metadata => {
   return {
-    title: `Menu ${params.productId}`
+    title: `Menu ${params.menu_id}`
   }
 }
 
 type Props = {
   params: {
-    productId: string;
+    menu_id: string;
   }
 }
 
 export default function Menu({params}: Props) {
   return (
-    <h1>Menu {params.productId}</h1>
+    <h1>Menu {params.menu_id}</h1>
   )
 }
