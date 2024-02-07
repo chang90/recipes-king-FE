37c67c7eb632c559217f9c32df518df7a6542d32
chang90 <chang.liu.programmer@gmail.com>
2024-02-07 21:08:01 +1100

    feat: create archived notification

--- src/app/[locale]/complex-dashboard/@notifications/archived/page.tsx
+++ src/app/[locale]/complex-dashboard/@notifications/archived/page.tsx
@@ -1,10 +1,10 @@
 import Card from "@/components/card/card";
 import Link from "next/link";
 
-export default function Notification() {
+export default function ArchivedNotifications() {
   return (
   <Card title="Notification">
-    <Link href="/en-AU/complex-dashboard/archived">Archived</Link>
+    <Link href="/en-AU/complex-dashboard">Default</Link>
   </Card>)
   
 }
