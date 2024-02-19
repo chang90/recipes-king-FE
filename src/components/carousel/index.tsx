efa10bfb397d8a47ff62765de6c76eca2f065a6a
chang90 <chang.liu.programmer@gmail.com>
2024-02-19 21:59:44 +1100

    feat: init carousel component (in progress)

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./carousel.module.scss";
+
+interface PropsType {
+  children: React.ReactNode;
+}
+
+const Carousel: React.FC<PropsType> = (props) => {
+  return (
+    <ul className={styles["carousel"]}>
+      {React.Children.map(props.children, (child, key) => {
+        return <li className={styles["carousel__item"]} key={key}>{child}</li>
+      })}
+    </ul>
+  );
+}
+
+export default Carousel;
