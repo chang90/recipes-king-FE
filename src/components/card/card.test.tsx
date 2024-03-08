efd6532aacc59f71d2ab01c352274e7f381829d8
chang90 <chang.liu.programmer@gmail.com>
2024-03-08 20:59:00 +1100

    feat: add test coverage script

--- src/components/card/card.test.tsx
+++ src/components/card/card.test.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { render, screen } from '@testing-library/react';
 import Card from "./card";
 
-test('renders header', () => {
-  render(<Card title="card title"/>);
-  const headerElement = screen.getByText(/card title/i);
-  expect(headerElement).toBeInTheDocument();
-})
-
-test('renders description', () => {
-  render(<Card title="card title" description="card description"/>);
-  const descriptionElement = screen.getByText(/card description/i);
-  expect(descriptionElement).toBeInTheDocument();
+describe('Card', () => {
+  test('renders header', () => {
+    render(<Card title="card title"/>);
+    const headerElement = screen.getByText(/card title/i);
+    expect(headerElement).toBeInTheDocument();
+  })
+  
+  test('renders description', () => {
+    render(<Card title="card title" description="card description"/>);
+    const descriptionElement = screen.getByText(/card description/i);
+    expect(descriptionElement).toBeInTheDocument();
+  })
 })
