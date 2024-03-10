4d99382a008812a4f4eac4a838b335863b4aee89
chang90 <chang.liu.programmer@gmail.com>
2024-03-10 18:48:58 +1100

    feat: level options for getByRole

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,10 +4,26 @@ import { Application } from "./Application";
 describe('Application', () => {
   test('render correctly', () => {
     render(<Application />);
+
+    const pageHeading = screen.getByRole('heading', {
+      level: 1
+    })
+    expect(pageHeading).toBeInTheDocument()
+
+    const sectionHeading = screen.getByRole('heading', {
+      level: 2
+    })
+    expect(pageHeading).toBeInTheDocument()
+
     const nameElement = screen.getByRole('textbox', {
       name: "Name"
     })
     expect(nameElement).toBeInTheDocument()
+
+    const bioElement = screen.getByRole('textbox', {
+      name: "Bio"
+    })
+    expect(bioElement).toBeInTheDocument()
 
     const jobLocationElement = screen.getByRole('combobox')
     expect(jobLocationElement).toBeInTheDocument()
