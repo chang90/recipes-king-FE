5be035c4a9e6244d8ab8137c6dbdc7106092e7b8
chang90 <chang.liu.programmer@gmail.com>
2024-03-11 21:33:29 +1100

    add skill component

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -10,26 +10,45 @@ describe('Application', () => {
     })
     expect(pageHeading).toBeInTheDocument()
 
+    const closeElement = screen.getByTitle('close')
+    expect(closeElement).toBeInTheDocument()
+
     const sectionHeading = screen.getByRole('heading', {
       level: 2
     })
-    expect(pageHeading).toBeInTheDocument()
+    expect(sectionHeading).toBeInTheDocument()
+
+    const paragraphElement = screen.getByText('All fields are mandatory')
+
+    expect(paragraphElement).toBeInTheDocument()
 
     const nameElement = screen.getByRole('textbox', {
       name: "Name"
     })
+
     expect(nameElement).toBeInTheDocument()
+
+    const nameElement2 = screen.getByLabelText('Name')
+
+
+    expect(nameElement2).toBeInTheDocument()
 
     const bioElement = screen.getByRole('textbox', {
       name: "Bio"
     })
     expect(bioElement).toBeInTheDocument()
 
+    const customElement = screen.getByTestId('custom-element')
+    expect(customElement).toBeInTheDocument()
+    
     const jobLocationElement = screen.getByRole('combobox')
     expect(jobLocationElement).toBeInTheDocument()
 
     const termsElement = screen.getByRole('checkbox')
     expect(termsElement).toBeInTheDocument();
+
+    const termsElement2 = screen.getByLabelText('I agree to the terms and conditions')
+    expect(termsElement2).toBeInTheDocument();
 
     const submitButtonElement = screen.getByRole('button')
     expect(submitButtonElement).toBeInTheDocument();
