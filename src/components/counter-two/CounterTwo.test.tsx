b1d197f6b00f626206bca6f5fa41bb450cf25302
chang90 <chang.liu.programmer@gmail.com>
2024-03-12 22:03:54 +1100

    feat:mock function with test

---
+++ src/components/counter-two/CounterTwo.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import user from '@testing-library/user-event'
+import { CounterTwo } from './CounterTwo'
+
+describe("CounterTwo", () => {
+  test('renders correctly', () => {
+    render(<CounterTwo count={0} />)
+    const textElement = screen.getByText('Counter Two')
+    expect(textElement).toBeInTheDocument()
+  })
+
+  test('handlers are called', async () => {
+    const incrementHandler = jest.fn()
+    const decrementHandler = jest.fn()
+    render(<CounterTwo
+      count={0}
+      handleDecrement={decrementHandler}
+      handleIncrement={incrementHandler}
+      />)
+
+    const incrementButton = screen.getByRole('button', {name: "Increment"})
+    const decrementButton = screen.getByRole('button', {name: "Decrement"})
+
+    await user.click(incrementButton);
+    await user.click(decrementButton);
+
+    expect(incrementHandler).toHaveBeenCalled;
+
+    expect(decrementHandler).toHaveBeenCalled;
+  })
+})
